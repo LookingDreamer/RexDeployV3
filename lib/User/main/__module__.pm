ad59bd4dde8e1d9821ff8cbf02cbe6ce7089d78f
黄高明 <530035210@qq.com>
2017-03-15 18:09:51 +0800

    新建用户完成

--- lib/User/main/__module__.pm
+++ lib/User/main/__module__.pm
@@ -23,6 +23,8 @@ task "route", sub {
 	my $action=$self->{action};
 	my $level=$self->{level};
 	my $sudo=$self->{sudo};
+	my $pass=$self->{pass};
+	my $allow=$self->{allow};
 	Rex::Logger::info("user参数: $user action参数: $action");
 	if ($user eq "" or $action eq "" ) {
 		Rex::Logger::info("用户名或动作不能为空","error");
@@ -34,7 +36,13 @@ task "route", sub {
 	if ($sudo eq "") {
 		$sudo = 0;
 	}	
-	Rex::Logger::info("level参数: $level sudo参数: $sudo");
+	if ($pass eq "") {
+		$pass = 0;
+	}
+	if ($allow eq "") {
+		$allow = 0;
+	}	
+	Rex::Logger::info("level参数: $level sudo参数: $sudo pass参数:$pass");
 	my @action_list = ('query' ,'create' ,'delete','lock');
 	my $action_status = $action ~~ @action_list;
 	my $action_string = join( ",", @action_list); 
@@ -46,31 +54,44 @@ task "route", sub {
 		queryUser($user);
 	}elsif($action eq "create"){
 		my $query = queryUser($user); 
-		createUser($user,$level,$query,$sudo);
+		createUser($user,$level,$query,$sudo,$pass,$allow);
 	}
 	
 };
 
 #level参数,密码认证:0代表普通用户密码为用户名+2017654321
-#level参数,秘钥认证:1有秘钥,秘钥是无密码的 sudo为1时,加入sudo
-#level参数,秘钥认证:2有秘钥,秘钥密码为用户名+2017654321 sudo为1时,加入sudo
+#level参数,秘钥认证:1有秘钥,秘钥是无密码的 sudo为1时,加入sudo 秘钥为默认的固定秘钥
+#level参数,秘钥认证:2有秘钥,秘钥是无密码的 sudo为1时,加入sudo 秘钥为随机生成rsa秘钥
+#level参数,秘钥认证:3有秘钥,秘钥是有密码的 sudo为1时,加入sudo 秘钥为随机生成rsa秘钥，且有密码
 # "创建用户";
 sub createUser{
 	my $user = @_[0];	
 	my $level = @_[1];	
 	my $query = @_[2];
+	my $sudo = @_[3];
+	my $pass = @_[4];
+	my $allow = @_[5];
 	if ( $query eq "0" ) {
 		if ( $level eq "0") {
 			create($user);
-		}elsif($level eq "1"){
+			if ($allow eq "1") {
+				append_allow($user);
+			}
+		}elsif($level eq "1" or $level eq "2" or $level eq "3"){
 			my $res = create($user);
 			if ( $res eq "0") {
 				exit;
 			}
-			create_authorized_keys($user);
+			create_authorized_keys($user,$level,$pass);
 			if ($sudo eq "1") {
 				create_sudo($user);
 			}
+			if ($allow eq "1") {
+				append_allow($user);
+			}			
+		}else{
+			Rex::Logger::info("level参数仅支持0,1,2,3","error");
+			exit;
 		}
 		
 	}else{
@@ -83,6 +104,7 @@ sub create{
 	my $user = @_[0];
 	my $cmd;
 	my $group_cmd = "egrep '\^$user\:' /etc/group &&  result=\$? ;echo status=\$result";
+	Rex::Logger::info("__SUB__:开始创建普通用户"); 
 	Rex::Logger::info("cmd:$group_cmd");
 	my $group = run "$group_cmd";
 	Rex::Logger::info("cmd用户组判断:$group"); 
@@ -104,9 +126,87 @@ sub create{
 	}	
 }
 
+sub general_key{
+	my $user = @_[0];
+	my $pass = @_[1];
+	my $cmd;
+	Rex::Logger::info("__SUB__:开始生成秘钥文件"); 
+	if ( $pass eq "0") {
+		$cmd = "mkdir -p /tmp/$user/$random ; ssh-keygen -t rsa -f /tmp/$user/$random/$user  -P '' &&  result=\$? ;echo status=\$result" ;
+	}else{
+		Rex::Logger::info("秘钥密码:$pass");
+		$cmd = "mkdir -p /tmp/$user/$random ; ssh-keygen -t rsa -f /tmp/$user/$random/$user  -P '$pass' &&  result=\$? ;echo status=\$result" ;
+	}
+	Rex::Logger::info("开始生成秘钥");
+	Rex::Logger::info("cmd:$cmd");
+	my $res = run "$cmd";
+	if ( $res =~ /status=0/ ) {
+		Rex::Logger::info("秘钥路径: /tmp/$user/$random/");
+		Rex::Logger::info("生成秘钥成功");
+		return 1;
+	}else{
+		Rex::Logger::info("生成秘钥失败","error");
+		exit;
+	}
+
+}
+
+
+sub append_allow{
+	my $user = @_[0];
+	my $cmd ;
+	my $cmd1 ;
+	my $cmd2 ;
+	$cmd1 = "cat /etc/ssh/sshd_config |grep AllowUsers |grep ' $user' &&  result=\$? ;echo status=\$result" ;
+	$cmd2 = "cat /etc/ssh/sshd_config |grep AllowUsers |grep ' $user\$' &&  result=\$? ;echo status=\$result" ;
+	$cmd = "sed -i 's/AllowUsers/AllowUsers $user /g' /etc/ssh/sshd_config" ;
+	Rex::Logger::info("__SUB__:开始添加allow至sshd配置文件"); 
+	Rex::Logger::info("开始把用户 $user 添加至allow");
+	Rex::Logger::info("cmd:$cmd");
+	my $res1 = run "$cmd1";
+	my $res2 = run "$cmd2";
+	if ( $res1 =~ /status=0/ or $res2 =~ /status=0/ ) {
+		Rex::Logger::info("该用户已经添加,无需重复添加.");
+		return 0;
+	}else{
+		my $res = run "$cmd";
+		if ( $res =~ /status=0/ ) {
+			Rex::Logger::info("添加成功");
+			return 1;
+		}else{
+			Rex::Logger::info("添加失败","error");
+			return 0;
+		}
+		
+	}		
+}
+
+
 sub create_authorized_keys{
 	my $user = @_[0];
-	my $cmd = "mkdir /home/$user/.ssh ;echo $common_public_key > /home/$user/.ssh/authorized_keys ;chown $user:$user  /home/$user -R &&  result=\$? ;echo status=\$result" ;
+	my $level = @_[1];
+	my $pass = @_[2];
+	my $cmd;
+	Rex::Logger::info("__SUB__:开始创建秘钥并写入用户ssh主目录"); 
+	if ( $level eq "1") {
+		$cmd = "mkdir /home/$user/.ssh ;echo $common_public_key > /home/$user/.ssh/authorized_keys ;chown $user:$user  /home/$user -R &&  result=\$? ;echo status=\$result" ;
+	}elsif($level eq "2"){
+		my $key = general_key($user);
+		my $common_public_key = run "cat /tmp/$user/$random/$user\.pub" ;
+		$cmd = "mkdir /home/$user/.ssh ;echo $common_public_key > /home/$user/.ssh/authorized_keys ;chown $user:$user  /home/$user -R &&  result=\$? ;echo status=\$result" ;
+	}elsif($level eq "3"){
+		my $key = general_key($user,$pass);
+		my $common_public_key = run "cat /tmp/$user/$random/$user\.pub" ;	
+		if ( $pass ne 0) {
+			if ( length($pass) < 8 ) {
+				Rex::Logger::info("密码为真时必须大于8个长度","error");
+				exit;
+			}
+		}
+		$cmd = "mkdir /home/$user/.ssh ;echo $common_public_key > /home/$user/.ssh/authorized_keys ;chown $user:$user  /home/$user -R &&  result=\$? ;echo status=\$result" ;	
+
+	}
+	
 	Rex::Logger::info("开始创建秘钥");
 	Rex::Logger::info("cmd:$cmd");
 	my $res = run "$cmd";
@@ -121,7 +221,8 @@ sub create_authorized_keys{
 
 sub create_sudo{
 	my $user = @_[0];
-	my $cmd = "usermod -g 10 $user" ;
+	my $cmd = "usermod -g 10 $user &&  result=\$? ;echo status=\$result" ;
+	Rex::Logger::info("__SUB__:开始添加sudo权限"); 
 	Rex::Logger::info("开始添加sudo/wheel");
 	Rex::Logger::info("cmd:$cmd");
 	my $res = run "$cmd";
@@ -139,6 +240,7 @@ sub queryUser{
 	my $user = @_[0];
 	my $server = Rex->get_current_connection()->{'server'};
 	my $res =  run "id $user && result=\$? ;echo status=\$result";
+	Rex::Logger::info("__SUB__:开始查询用户"); 
 	Rex::Logger::info("当前服务器: $server");
 	Rex::Logger::info("返回命令结果: $res");
 	if ( $res =~ /status=0/ ) {
