727214cc356d7890786d7dba73425d7adee0268d
般若菠萝蜜 <530035210@qq.com>
2018-03-21 01:07:35 +0800

    add docker

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
-
+rm /var/lib/mysql/* -rf
+mysql_install_db --user=mysql
 mysqld_safe &
 mysqladmin -u root password 'root' 
 mysql -u root -p 'root'  -e 'CREATE DATABASE autotask DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci'
