c3bd3831cdc7874966ae0bd5182888fa8a9fb57f
般若菠萝蜜 <530035210@qq.com>
2018-03-21 00:55:52 +0800

    docker修改

---
+++ docker/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+mysqld_safe &
+mysqladmin -u root password 'root' 
+mysql -u root -p 'root'  -e 'CREATE DATABASE autotask DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci'
+/bin/bash  install/dockerinit.sh setConfig
+mysqladmin -u root -p 'root' shutdown
+mysqld_safe
