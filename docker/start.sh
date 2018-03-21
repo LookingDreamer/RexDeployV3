0a03f9b74c92aad99686f9a2c9d20bcad81c5cb7
黄高明 <530035210@qq.com>
2018-03-21 12:08:35 +0800

    优化结构以及数据修正

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
+/bin/bash install/initStall.sh instalib 
 rm /var/lib/mysql/* -rf
 mysql_install_db --user=mysql
-mysqld_safe &
+cd /usr ; /usr/bin/mysqld_safe &
 mysqladmin -u root password 'root' 
-mysql -u root -p 'root'  -e 'CREATE DATABASE autotask DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci'
+mysql -uroot -p'root'  -e 'CREATE DATABASE autotask DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci'
 /bin/bash  install/dockerinit.sh setConfig
 mysqladmin -u root -p 'root' shutdown
-mysqld_safe
+cd /usr ; /usr/bin/mysqld_safe 
