90e73769037555f3cbaa1b32fb8d91eaeb2568fd
般若菠萝蜜 <530035210@qq.com>
2018-03-23 00:32:28 +0800

    docker 完善

--- docker/start.sh
+++ docker/start.sh
@@ -1,15 +1,4 @@
 #!/bin/bash 
-rm /var/lib/mysql/* -rf 
-mysql_install_db --user=mysql
-cd /usr ; /usr/bin/mysqld_safe &
-sleep 2
-mysqladmin -u root password 'root' 
-mysql -uroot -p'root'  -e 'CREATE DATABASE autotask DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci'
-cd /data/RexDeployV3
-/bin/bash install/dockerinit.sh setConfig
-mysqladmin -uroot -p'root' shutdown
-adduser autotask
-echo "autotask" |passwd --stdin  autotask
 /usr/sbin/sshd -D &
 cd /usr ; /usr/bin/mysqld_safe 
 
